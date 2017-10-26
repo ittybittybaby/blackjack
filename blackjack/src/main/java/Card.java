9155f1aca08770dbec8061ee653edcdd10fa85be
rikeroleg <rikeroleg@gmail.com>
2017-10-26 15:51:16 -0400

    I think were done :)

--- blackjack/src/main/java/Card.java
+++ blackjack/src/main/java/Card.java
@@ -1,2 +1,18 @@
 public class Card {
+    private Rank rank;
+    private Suit suit;
+
+    public Card(Rank rank, Suit suit) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+
+    public int getRankValue() {
+        return rank.getValue();
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
 }
