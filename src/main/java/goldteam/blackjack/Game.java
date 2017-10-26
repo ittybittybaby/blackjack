aa2a6531a31a4af6fb9c568a51aad2e1c27a827e
logan <loganhanner@gmail.com>
2017-10-26 14:40:59 -0400

    Updated

---
+++ src/main/java/goldteam/blackjack/Game.java
@@ -0,0 +1,44 @@
+package goldteam.blackjack;
+
+import java.util.ArrayList;
+
+public class Game {
+
+    private ArrayList<Player> players = new ArrayList<Player>();
+    private Player activePlayer;
+    private static final int  BLACKJACK = 21;
+
+    public Game() {
+        initializeGame();
+    }
+
+    protected void initializeGame() {
+        Deck deck = new Deck();
+        Player dealer = new Player();
+        Player player = new Player();
+        players.add(dealer);
+        players.add(player);
+
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public Player getActivePlayer() {
+        return activePlayer;
+    }
+
+    public void endTurn(Player activePlayer) {
+        for(Player player:players) {
+            if(!activePlayer.equals(player))
+                this.activePlayer = player;
+        }
+    }
+
+
+
+    //public
+
+
+}
