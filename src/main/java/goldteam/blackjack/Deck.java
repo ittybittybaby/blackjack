11503856aa2b1a0178cfeebd7dc1613cbd054f82
logan <loganhanner@gmail.com>
2017-10-26 18:44:14 -0400

    bwahhh

--- src/main/java/goldteam/blackjack/Deck.java
+++ src/main/java/goldteam/blackjack/Deck.java
@@ -1,5 +1,53 @@
 package goldteam.blackjack;
 
-public class Deck {
+import goldteam.blackjack.Card;
+import goldteam.blackjack.Rank;
+import goldteam.blackjack.Suit;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+
+public class Deck{
+
+    private ArrayList<Card> cards = new ArrayList<Card>(52);
+
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public int getDeckSize(Deck deck) {
+        return deck.cards.size();
+    }
+
+    public void shuffleDeck(Deck deck) {
+        ArrayList<Card> cards = deck.getCards();
+        Collections.shuffle(cards);
+
+    }
+
+    public void populate() {
+        ArrayList<Card> cards = new ArrayList();
+
+        for (int i = 0; i < 4; i++) {
+            Suit[] suitArray = new Suit[]{Suit.CLUB, Suit.DIAMOND, Suit.HEART, Suit.SPADE};
+            for (int j = 1; j < 13; j++) {
+                Rank[] rankArray = new Rank[]{Rank.ACE, Rank.TWO, Rank.THREE, Rank.FOUR, Rank.FIVE, Rank.SIX, Rank.SEVEN, Rank.EIGHT, Rank.NINE, Rank.TEN, Rank.JACK, Rank.QUEEN, Rank.KING};
+
+                /*Suit suit = array[i];
+                Rank rank = Rank();*/
+                Card card = new Card(rankArray[j], suitArray[i]);
+                cards.add(card);
+
+            }
+
+
+        }
+
+    }
 
 }
+
