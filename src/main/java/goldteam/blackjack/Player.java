09f3b3ad334d93014d30f439c39832b18b8e652e
elliott <elliott@zipcoders-MacBook-Pro.local>
2017-10-26 16:04:06 -0400

    playerUpdate

--- src/main/java/goldteam/blackjack/Player.java
+++ src/main/java/goldteam/blackjack/Player.java
@@ -1,4 +1,52 @@
-package goldteam.blackjack;
+package kozulak.elliott.blackjack;
+
+import kozulak.elliott.blackjack.Card;
+
+import java.util.ArrayList;
 
 public class Player {
+
+    private String name;
+    private Double money;
+    private ArrayList<Card> cardsInHand;
+
+    public Player (String name, Double money){
+        this.name=name;
+        this.money=money;
+        this.cardsInHand=new ArrayList<Card>();
+    }
+
+
+    public String getName(){return name;}
+
+    public Double checkBalance(){return money;}
+
+    public Double wager(Double wager){
+        money-=wager;
+        return wager;}
+
+    public void addCardtoHand(Card newCard){
+        cardsInHand.add(newCard);
+    }
+
+    public String viewHand() {
+        String handString="";
+        for (Card card : cardsInHand) {
+            handString+= card.toString();
+        }
+        return handString;
+    }
+
+    public int getHandValue(){
+        int total=0;
+        for(Card card:cardsInHand){
+            total+=card.getRankValue();}
+        return total;
+    }
+
+    public ArrayList<Card> getHandList(){
+        return cardsInHand;
+    }
+
+
 }
