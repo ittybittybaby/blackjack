8963a2dadc53cd108118fb290ab1cdc01dbaf223
elliott <elliott@zipcoders-MacBook-Pro.local>
2017-10-26 20:19:35 -0400

    addedMethods

--- src/main/java/goldteam/blackjack/Player.java
+++ src/main/java/goldteam/blackjack/Player.java
@@ -1,48 +1,70 @@
 package goldteam.blackjack;
+
 import java.util.ArrayList;
 
 public class Player {
 
     private String name;
-    private Double money;
+    private Double cash;
     private ArrayList<Card> cardsInHand;
 
-    public Player (String name, Double money){
+    public Player(String name, Double cash) {
         this.name = name;
-        this.money=money;
-        this.cardsInHand=new ArrayList<Card>();
+        this.cash = cash;
+        this.cardsInHand = new ArrayList<Card>();
     }
 
 
-    public String getName(){return name;}
+    public String getName() {
+        return name;
+    }
 
-    public Double checkBalance(){return money;}
+    public Double checkBalance() {
+        return cash;
+    }
 
-    public Double wager(Double wager){
-        money-=wager;
-        return wager;}
+    public Double wager(Double wager) {
+        cash -= wager;
+        return wager;
+    }
 
-    public void addCardtoHand(Card newCard){
+    public void addCardtoHand(Card newCard) {
         cardsInHand.add(newCard);
     }
 
     public String viewHand() {
-        String handString="";
+        String handString = "";
         for (Card card : cardsInHand) {
-            handString+= card.toString();
+            handString += card.toString();
         }
         return handString;
     }
 
-    public int getHandValue(){
-        int total=0;
-        for(Card card:cardsInHand){
-            total+=card.getRankValue();}
+    public int getHandValue() {
+        int total = 0;
+        int acesCount = 0;
+
+        for (Card card : cardsInHand) {
+            if (card.getRankValue() == 11)
+                acesCount++;
+            total += card.getRankValue();
+        }
+
+        if (total > 21) {
+            while (acesCount > 0) {
+                total -= 10;
+                acesCount--;
+            }
+        }
         return total;
     }
 
-    public ArrayList<Card> getHandList(){
+    public ArrayList<Card> getHandList() {
         return cardsInHand;
+    }
+
+    public void addWinnings(Double winnings) {
+        cash += winnings;
     }
 
 
